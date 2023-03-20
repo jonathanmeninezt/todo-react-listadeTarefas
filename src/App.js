91a27719471bcc133e043322594314869d07604d
Jonathan Meninez <jonathan.meninezt@gmail.com>
2023-03-20 20:04:15 -0300

    criação- todo-projetos de tarefas- React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <section id="app" className="container">
+    <header>
+      <h1 className="title">Todo</h1>
+    </header>
+    <section className="main">
+     <input className="new-todo" placeholder="O que precisa ser feito?"/>
+    </section>
+  </section>
+);
+
 
 export default App;
